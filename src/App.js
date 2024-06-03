5d039ba0ece71212f63e3b43859b5d73e42ca124
Alisa Raevskaya <bhumi.alisa@gmail.com>
2024-06-03 18:21:48 +0400

    Tweak watches

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useState, useEffect, memo } from "react";
+
+
+const formatTime = (date) => {
+  const d = new Date(date).toLocaleString('ge-GE');
+  return `${d.slice(11, 20)}`;
+};
+
+
+
+const ImageComponent = memo(function ImageComponent({ ...isRound }) {
+  const [url, setUrl] = useState("https://picsum.photos/200/300");
+
+  useEffect(() => {
+    if (isRound) {
+      // console.log("round", url);
+      //fetch("https://picsum.photos/200/300").then((data) => setUrl(data.url));
+    }
+  });
+
+  // console.log(url);
+  return (
+    <>
+      <div className="img">
+        <img src={url} alt="img" />
+      </div>
+    </>
+  );
+});
+
+export default function App() {
+  const [time, setTime] = useState(formatTime(new Date()));
+  const [isUTC, setIsUTC] = useState(false);
+  // const [isRound, setIsRound] = useState(false);
+  // const checkedValue = isUTC ? "checked" : "";
+
+  useEffect(() => {
+    let start = Date.now();
+
+    const myTimeout = setTimeout(() => {
+      const delayMargin = Date.now() - start;
+      let diff = delayMargin - 1000;
+      setTime(formatTime(new Date( Date.now() - diff)));
+    }, 1000);
+
+    //console.log(newMilliseconds);
+
+    // setIsRound(checkIfSecondsAreRound(isUTC, date));
+
+    return () => {
+      clearTimeout(myTimeout);
+    };
+  });
+
+  const handleCheckbox = () => {
+    setIsUTC(!isUTC);
+  };
+
+  //console.log("render", time);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="heading-1">{time}</h1>
+        {/* 
+        {/* <div className="row">
+          <p>Switch to UTC</p>
+          <input
+            type="checkbox"
+            className="utc-checkbox"
+            checked={checkedValue}
+            onChange={handleCheckbox}
+          />
+        </div> */}
+
+        {/* <ImageComponent isRound={isRound} /> */}
+      </div>
     </div>
   );
 }
-
-export default App;
